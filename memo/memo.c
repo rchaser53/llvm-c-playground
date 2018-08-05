9d030a70d3c338530aab406c9cdaeb7b1715ea62
rchaser53 <tayoshizawa29@gmail.com>
2018-08-05 23:36:07 +0900

    add the memo for how to use dynamic array in c

---
+++ memo/memo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  // auto int *heap;
+  // heap = (int *)malloc(sizeof(int) * 10);
+  // heap = (int *)realloc(heap,sizeof(int) * 100);
+  // heap[11] = 10;
+  // printf("%d\n", heap[11]);
+  // free(heap);
+}
