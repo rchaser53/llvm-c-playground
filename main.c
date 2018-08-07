e96eb0944124598756181db982cd9aebc30ba773
rchaser53 <tayoshizawa29@gmail.com>
2018-08-07 22:35:43 +0900

    emit and execute llvm ir

--- main.c
+++ main.c
@@ -23,16 +23,18 @@ LLVMValueRef llvmGenLocalStringVar(LLVMModuleRef mod, const char* data, int len)
 int main(int argc, char const *argv[]) {
   LLVMModuleRef mod = LLVMModuleCreateWithName("my_module");
 
-  LLVMTypeRef param_types[] = { LLVMInt32Type(), LLVMInt32Type() };
-  LLVMTypeRef ret_type = LLVMFunctionType(LLVMInt32Type(), param_types, 2, 0);
-  LLVMValueRef sum = LLVMAddFunction(mod, "main", ret_type);
+  // LLVMTypeRef param_types[] = { LLVMInt32Type(), LLVMPointerType(LLVMPointerType(LLVMInt8Type(), 0), 0) };
+  // LLVMTypeRef ret_type = LLVMFunctionType(LLVMInt32Type(), param_types, 2, 0);
+  LLVMTypeRef param_types[] = { LLVMInt32Type() };
+  LLVMTypeRef ret_type = LLVMFunctionType(LLVMInt32Type(), param_types, 1, 0);
+  LLVMValueRef main = LLVMAddFunction(mod, "main", ret_type);
 
   // what is an address space?
   LLVMTypeRef nyans[] = { LLVMPointerType(LLVMInt8Type(), 0) };
   LLVMTypeRef rettype = LLVMFunctionType(LLVMInt32Type(), nyans, 1, 0);
   LLVMValueRef puts_fn = LLVMAddFunction(mod, "puts", rettype);
 
-  LLVMBasicBlockRef entry = LLVMAppendBasicBlock(sum, "entry");
+  LLVMBasicBlockRef entry = LLVMAppendBasicBlock(main, "entry");
 
   LLVMContextRef context = LLVMGetGlobalContext();
   LLVMBuilderRef builder = LLVMCreateBuilderInContext(context);
@@ -41,7 +43,7 @@ int main(int argc, char const *argv[]) {
   LLVMTypeRef te = LLVMInt32Type();
   LLVMValueRef val = LLVMBuildAlloca(builder, te, "uei");
   LLVMBuildStore(builder, LLVMConstInt(te, 31, 0), val);
-  LLVMValueRef tmp = LLVMBuildAdd(builder, LLVMGetParam(sum, 0), LLVMBuildLoad(builder, val, "uei"), "tmp");
+  LLVMValueRef tmp = LLVMBuildAdd(builder, LLVMGetParam(main, 0), LLVMBuildLoad(builder, val, "uei"), "tmp");
 
   LLVMValueRef GlobalVar = LLVMAddGlobal(mod, LLVMArrayType(LLVMInt8Type(), 6), "simple_value");
   LLVMValueRef TempStr = LLVMConstString("nyan", 6, 1);
@@ -71,13 +73,16 @@ int main(int argc, char const *argv[]) {
     fprintf(stderr, "failed to create execution engine\n");
     abort();
   }
+
   if (error) {
     fprintf(stderr, "error: %s\n", error);
     LLVMDisposeMessage(error);
     exit(EXIT_FAILURE);
   }
-
   LLVMDumpModule(mod);
+
+  LLVMGenericValueRef exec_args[] = { LLVMCreateGenericValueOfInt(LLVMInt32Type(), 1, 0)};
+  LLVMRunFunction(engine, main, 1, exec_args);
 
   LLVMDisposeBuilder(builder);
   LLVMDisposeExecutionEngine(engine);
